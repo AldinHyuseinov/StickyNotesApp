c4c92b85ae771f022d86ae931c25faafe5b62e5c
AldinHyuseinov <Aldin6687@gmail.com>
2023-05-01 21:14:51 +0300

    Working login, register and logout.

--- notes-frontend/src/pages/Home.js
+++ notes-frontend/src/pages/Home.js
@@ -3,8 +3,8 @@ function Home() {
         <main className="container my-5">
             <div className="row">
                 <h1 className="text-center">Welcome to Notes!</h1>
-                <h2 className="text-center"><a href="/auth/login">Login</a> to access and create your own notes!</h2>
-                <h2 className="text-center"><a href="/auth/register">Register</a> if you don't have an account.</h2>
+                <h2 className="text-center"><a className="link-warning" href="/auth/login">Login</a> to access and create your own notes!</h2>
+                <h2 className="text-center"><a className="link-warning" href="/auth/register">Register</a> if you don't have an account.</h2>
             </div>
         </main>
     );
